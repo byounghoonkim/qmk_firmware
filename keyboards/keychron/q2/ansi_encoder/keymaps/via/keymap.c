24e9355e8280afd8f212e1ef35a40f5f6ae549b6
Byounghoon Kim <blue021@gmail.com>
2025-01-06 12:44:52 +0900

    fix: change keymap homerow for q2 knob

--- keyboards/keychron/q2/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/via/keymap.c
@@ -27,14 +27,24 @@ enum layers {
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+#define KC_A_HR MT(MOD_LGUI, KC_A)
+#define KC_S_HR MT(MOD_LALT,KC_S)
+#define KC_D_HR MT(MOD_LSFT,KC_D)
+#define KC_F_HR MT(MOD_LCTL,KC_F)
+#define KC_G_HR MT(MOD_LCTL | MOD_LSFT | MOD_LALT,KC_G)
+#define KC_J_HR MT(MOD_LCTL | MOD_RCTL,KC_J)
+#define KC_K_HR MT(MOD_LSFT | MOD_RSFT,KC_K)
+#define KC_L_HR MT(MOD_LALT | MOD_RALT,KC_L)
+#define KC_SCLN_HR MT(MOD_LGUI | MOD_RGUI,KC_SCLN)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_67(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           KC_HOME,
+        KC_LCTL, KC_A_HR,  KC_S_HR,  KC_D_HR, KC_F_HR, KC_G_HR, KC_H,    KC_J_HR, KC_K_HR, KC_L_HR, KC_SCLN_HR,KC_QUOT,           KC_ENT,           KC_MNXT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
+        MO(_FN1), KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_RCMD, MO(_FN1), MO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_ansi_67(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
@@ -46,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN1] = LAYOUT_ansi_67(
         KC_GRV,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
-        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,            _______,          _______,
+        _______, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,            _______,          _______,
         _______,          _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,            _______, _______,
         _______, _______, _______,                            _______,                            _______, _______,  _______,  _______, _______, _______),
 
